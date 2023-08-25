4404382f92e4a31a3852baec979d559703e5ebbc
Iñaki Baz Castillo <ibc@aliax.net>
2023-08-25 21:45:23 +0200

    Add readSigned3BytesInDataView() and writeSigned3BytesInDataView() utils

--- src/utils/byteOps.ts
+++ src/utils/byteOps.ts
@@ -13,7 +13,7 @@ export function read3BytesInDataView(
 }
 
 /**
- * Write a unsigned value in 3 bytes starting from byte `pos` in `view`.
+ * Write an unsigned value in 3 bytes starting from byte `pos` in `view`.
  *
  * @category Utils
  * @hidden
@@ -24,5 +24,41 @@ export function write3BytesInDataView(
 ): void
 {
 	view.setUint8(pos, value >> 16);
+	view.setUint16(pos + 1, value);
+}
+
+/**
+ * Read 3 bytes starting from byte `pos` in `view` as signed value.
+ *
+ * @category Utils
+ * @hidden
+ */
+export function readSigned3BytesInDataView(
+	{ view, pos }:
+	{ view: DataView; pos: number }
+): number
+{
+	const byte2 = view.getUint8(pos);
+	const byte1 = view.getUint8(pos + 1);
+	const byte0 = view.getUint8(pos + 2);
+
+	// Check bit 7 (sign).
+	const extension = byte2 & 0b10000000 ? 0b11111111 : 0b00000000;
+
+	return byte0 | (byte1 << 8) | (byte2 << 16) | (extension << 24);
+}
+
+/**
+ * Write a signed value in 3 bytes starting from byte `pos` in `view`.
+ *
+ * @category Utils
+ * @hidden
+ */
+export function writeSigned3BytesInDataView(
+	{ view, pos, value }:
+	{ view: DataView; pos: number; value: number }
+): void
+{
+	view.setInt8(pos, value >> 16);
 	view.setUint16(pos + 1, value);
 }
